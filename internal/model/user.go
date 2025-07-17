dc5e82020a964003215b037e5f290669b48d5593
Lenya Nab <nabiev.lenya@gmail.com>
2025-07-17 21:55:43 +0400

    Update user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,11 @@
 package model
 
+import "time"
+
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID        uint      `json:"id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
