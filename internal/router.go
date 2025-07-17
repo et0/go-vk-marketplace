6b31d973c50e374ff115d1b3def2302686fe5420
Lenya Nab <nabiev.lenya@gmail.com>
2025-07-17 21:57:49 +0400

    Add new router /register

--- internal/router.go
+++ internal/router.go
@@ -1,9 +1,22 @@
 package router
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/et0/go-vk-marketplace/internal/handler"
+	"github.com/et0/go-vk-marketplace/internal/service"
+	"github.com/et0/go-vk-marketplace/internal/storage"
+	"github.com/labstack/echo/v4"
+)
 
-func New() *echo.Echo {
+func New(db storage.Database) *echo.Echo {
 	e := echo.New()
+
+	// Инициализация сервисов для работы с бд
+	userService := service.NewUserService(db)
+
+	// Инициализация хендлеров
+	userHandler := handler.NewUserHandler(userService)
+
+	e.POST("/register", userHandler.Register)
 
 	return e
 }
