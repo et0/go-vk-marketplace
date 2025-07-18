09fde15c00397e6590a6ece014f5991939f85f8f
Lenya Nab <nabiev.lenya@gmail.com>
2025-07-18 04:35:46 +0400

    Add new router /ads

--- internal/router.go
+++ internal/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/et0/go-vk-marketplace/internal/handler"
+	"github.com/et0/go-vk-marketplace/internal/middleware"
 	"github.com/et0/go-vk-marketplace/internal/service"
 	"github.com/et0/go-vk-marketplace/internal/storage"
 	"github.com/labstack/echo/v4"
@@ -10,15 +11,24 @@ import (
 func New(db storage.Database, jwtSecret string) *echo.Echo {
 	e := echo.New()
 
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+
+	e.Use(middleware.CheckToken(jwtSecret))
+
 	// Инициализация сервисов для работы с бд
 	userService := service.NewUserService(db)
+	adService := service.NewAdService(db)
 
 	// Инициализация хендлеров
 	userHandler := handler.NewUserHandler(userService)
 	authHandler := handler.NewAuthHandler(userService, jwtSecret)
+	adHandler := handler.NewAdHandler(userService, adService)
 
 	e.POST("/signup", userHandler.Signup)
 	e.POST("/login", authHandler.Login)
+	// e.GET("/ads", adHandler.GetAll)
+	e.POST("/ads", adHandler.Create, middleware.IsAuth)
 
 	return e
 }
