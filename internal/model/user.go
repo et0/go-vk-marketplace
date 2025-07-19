37d96887c7f95bd6d299a297f1e38e76af7e2998
Lenya Nab <nabiev.lenya@gmail.com>
2025-07-19 19:05:09 +0400

    Update validate max value

--- internal/model/user.go
+++ internal/model/user.go
@@ -20,6 +20,6 @@ type LoginResponse struct {
 }
 
 type SignupRequest struct {
-	Username string `json:"username" validate:"required,min=3,max=64"`
+	Username string `json:"username" validate:"required,min=3,max=32"`
 	Password string `json:"password" validate:"required,min=8,max=32"`
 }
